88354e60752e65fa6010484d141e08a165272e72
starktk <104461019+starktk@users.noreply.github.com>
2022-09-09 21:34:26 -0300

    1 file modified, 1 added

--- 0.1.2.js
+++ 0.1.2.js
@@ -1,20 +1,42 @@
-const compra = 100;
-const cartao = false;
-const convenio = true;
-const cartaoEconvenio = cartao && convenio;
-const cartaoOUconvenio = cartao || convenio;
 
-function venda(compra, cartaoEconvenio, cartaoOUconvenio) {
-  const descontoT = compra * 0.15;
-  const descontoM = compra * 0.10;
+function venda(valorCompra, cartao, convenio, primeiraCompra) {
 
-  if (cartaoEconvenio) {
-    console.log(compra - descontoT);
-  } else if (cartaoOUconvenio) {
-    console.log(compra - descontoM);
-  } else {
-    console.log(compra);
+
+
+ if (primeiraCompra) {
+  let descontoFirstBuy = valorCompra * 0.05;
+  if (descontoFirstBuy > 100) {
+    descontoFirstBuy = 100;
   }
+    const descontoPC = valorCompra - descontoFirstBuy;
+    console.log(descontoPC);
+
+    return descontoPC;
+  } else {
+ 
+  if (cartao && convenio) {
+    let descontoTotal = valorCompra * 0.15;
+    if (descontoTotal > 100) {
+      descontoTotal = 100;
+     }
+    const descontT = valorCompra - descontoTotal;
+    console.log(descontT);
+
+    return descontT;
+  } else if (cartao || convenio ) {
+    let descontoMeio = valorCompra * 0.10;
+    if (descontoMeio > 100) {
+      descontoMeio = 100;
+    }
+    const descontM = valorCompra - descontoMeio;
+    console.log(descontM);
+
+    return descontM;
+  } else {
+    console.log(valorCompra);
+  }
+ 
+ } 
 }
 
-venda(compra, cartaoEconvenio, cartaoOUconvenio);
+venda(1000, true, true, false);
