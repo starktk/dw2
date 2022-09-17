6da15fdd2851d8d75dcab196c5c76a445467eaa3
starktk <104461019+starktk@users.noreply.github.com>
2022-09-16 21:19:48 -0300

    1 file added

---
+++ Aula 3/0.2.1.js
@@ -0,0 +1,32 @@
+const filmfav = {
+
+     StepUpAllin: {
+      year: 2014,
+      de: 'Eua',
+      genero = [ 
+        'musical',
+      ]
+
+    },
+
+      GOT: {
+      seasons: 8,
+      launchyear: 2011,
+      genero = [
+      
+        'drama',
+      ]
+
+    },
+
+    TWD: {
+      seasons: 11,
+      launchyear: 2010,
+      genero = [
+        'drama',
+        'drama',
+      ]
+
+    }
+}
+console.log(filmfav);
